0e1910d55bae1c5eb1f60fe3f9e7094017cd8ff7
laugeeme <registro.laura@gmail.com>
2020-07-20 19:02:09 +0200

    save dates in the database with post

--- API/controllers/patientControllers.js
+++ API/controllers/patientControllers.js
@@ -1,8 +1,16 @@
 'use strict';
+const Patient = require('../models/Patient');
 
 //when a new client is created
-exports.newClient = (req, res, next) => {
-  console.log(req.body);
+exports.newClient = async (req, res, next) => {
+  //create patient object with data from req.body
+  const patient = new Patient(req.body);
 
-  res.json({ message: 'El cliente se agregó correctamente' });
+  try {
+    await patient.save();
+    res.json({ message: 'El cliente se agregó correctamente' });
+  } catch (error) {
+    console.log(error);
+    next(); //to go to the next function if there is an error
+  }
 };
