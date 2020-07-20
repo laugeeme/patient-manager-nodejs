8c44e6bdb8f3e8c6478d331948cc28c1cf42b46f
laugeeme <registro.laura@gmail.com>
2020-07-20 18:07:22 +0200

    connect to mongodb

--- API/index.js
+++ API/index.js
@@ -1,8 +1,18 @@
 'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+
 
 //create server with Express
-const express = require('express');
 const app = express();
+
+//connect to Mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinary', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 //port and run server
 app.listen(4000, () => {
