2215608a4ec225c86a5b640ac7514d8d54bf8e1b
laugeeme <registro.laura@gmail.com>
2020-07-21 13:20:28 +0200

    add structure and style to newappointment component

--- patient-manager/src/components/NewAppointment.js
+++ patient-manager/src/components/NewAppointment.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const NewAppointment = () => {
-    return ( <h1>From new Appointement</h1> );
-}
- 
+  return (
+    <Fragment>
+      <h1 className="my-5">New Appointment</h1>
+      <div className="container mt-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5 d-flex justify-content-center">
+            <Link
+              to={'/'}
+              className="btn btn-success text-uppercase py-2 px-5 font-weight-bold"
+            >
+              Back{' '}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
 export default NewAppointment;
--- patient-manager/src/components/Patients.js
+++ patient-manager/src/components/Patients.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Patients = ({ appointments }) => {
   if (appointments.length === 0) return null;
@@ -11,12 +12,12 @@ const Patients = ({ appointments }) => {
       <div className="container mt-5 py-5">
         <div className="row">
           <div className="col-12 mb-5 d-flex justify-content-center">
-            <a
-              href="#"
+            <Link
+              to={'/new'}
               className="btn btn-success text-uppercase py-2 px-5 font-weight-bold"
             >
               New appointment
-            </a>
+            </Link>
           </div>
 
           <div className="col-md-8 mx-auto">
