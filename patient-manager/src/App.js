4e42c0e1bb1baf629ea0ef576cccf0a5082fc098
laugeeme <registro.laura@gmail.com>
2020-07-21 12:03:19 +0200

    import the client axios and use hooks to see the state of the app

--- patient-manager/src/App.js
+++ patient-manager/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Patients from './components/Patients';
 import NewAppointment from './components/NewAppointment';
 import Appointment from './components/Appointment';
 
-function App() {
+import clientAxios from './config/axios';
 
-  
+function App() {
+  //app state
+  const [appointments, saveAppointments] = useState([]);
+
+  useEffect(() => {
+    //From here we will consume our external api
+    const consultApi = () => {
+      clientAxios
+        .get('/patients')
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    consultApi();
+  }, []);
 
   return (
     <Router>
